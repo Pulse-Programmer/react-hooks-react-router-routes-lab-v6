82886a4e9f667cf9fe0200b53b9a245218d5e146
Stephen Ochieng <materiamystic@gmail.com>
2024-05-16 14:37:08 +0300

    Adds required features

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,18 @@
+import NavBar from "../components/NavBar";
+
+function ErrorPage() {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <div>
+          <h1>Oops! Looks like something went wrong.</h1>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default ErrorPage;
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,36 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import { useParams } from "react-router-dom";
 
 function Movie() {
+  const { id } = useParams();
+
+  const [details, setDetails] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then((res) => res.json())
+      .then((data) => setDetails(data));
+  }, [id]);
+
+  if (!details) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{details.title}</h1>
+        <p>{details.time}</p>
+        {details.genres.map((genre) => (
+          <span key={genre}>{genre}</span>
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Movie;
